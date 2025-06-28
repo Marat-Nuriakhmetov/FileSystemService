de7f2f3645deb455976ba67631226dde3b11f40e
Marat Nuriakhmetov <nmarat@amazon.com>
2025-06-28 23:13:50 +0200

    add gradle and gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+}
+
+group = "jb.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    flatDir {
+        dirs("libs")
+    }
+}
+
+dependencies {
+    implementation("com.github.arteam:simple-json-rpc-server:1.2")
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("org.eclipse.jetty:jetty-server:11.0.18")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.18")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
